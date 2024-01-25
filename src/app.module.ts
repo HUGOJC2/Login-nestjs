9e7f4b7ec7637d25ce253dc9ca305782eac0d514
HUGOJC2 <157401501+HUGOJC2@users.noreply.github.com>
2024-01-25 13:40:26 -0600

    Actulizacion

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_BASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}', __dirname + '/**/**/*.entity{.ts,.js}' ],
+      synchronize: true,
+      logging: ["query", "error"]
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
